72dcceb5f9ae74e5ff68cfd04e602a1fbd811ebf
Padrao <padrao@dev01.dngbh.local>
2021-08-10 11:23:06 -0300

    Protegendo informaçoes pessoais com variaveis de ambiente

--- src/config/discordAuth.ts
+++
@@ -1,6 +0,0 @@
-export const REDIRECT_URI = "exp%3A%2F%2F192.168.200.138%3A19000";
-export const SCOPE = "identify%20email%20connections%20guilds";
-export const RESPONSE_TYPE = "token";
-export const CLIENT_ID = "872826568483946627";
-export const CDN_IMAGE = "https://cdn.discordapp.com";
-
--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,7 +1,13 @@
 import React, { createContext, useContext, useState, ReactNode } from "react";
 import * as AuthSession from "expo-auth-session"
 import { api } from "../services/api";
-import { CDN_IMAGE, CLIENT_ID, REDIRECT_URI, RESPONSE_TYPE, SCOPE } from "../config/discordAuth";
+
+const {CDN_IMAGE} = process.env
+const {CLIENT_ID} = process.env
+const {REDIRECT_URI} = process.env
+const {RESPONSE_TYPE} = process.env
+const {SCOPE } = process.env
+
 
 
 type User = {
